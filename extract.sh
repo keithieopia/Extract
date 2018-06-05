1afe76fec35b03d62ba9eb6c328f904f02e1a538
Timothy Keith <timothykeith@gmail.com>
2018-06-05 14:40:21 -0400

    use magic numbers in file vs. it's file extension to extract

--- extract.sh
+++ extract.sh
@@ -10,20 +10,33 @@ else
     for n in "$@"
     do
         if [ -f "$n" ] ; then
-            case "${n%,}" in
-                *.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar) 
-                             tar xvf "$n"       ;;
-                *.lzma)      unlzma ./"$n"      ;;
-                *.bz2)       bunzip2 ./"$n"     ;;
-                *.rar)       unrar x -ad ./"$n" ;;
-                *.gz)        gunzip ./"$n"      ;;
-                *.zip)       unzip ./"$n"       ;;
-                *.z)         uncompress ./"$n"  ;;
-                *.7z|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar)
-                             7z x ./"$n"        ;;
-                *.xz)        unxz ./"$n"        ;;
-                *.exe)       cabextract ./"$n"  ;;
-                *.cpio)      cpio -id < ./"$n"  ;;
+            case $(file -b --mime-type "$n") in
+                application/x-bzip2|application/x-gzip|application/x-xz|application/x-tar) 
+                    tar xvf "$n"       ;;
+                application/x-lzma)      
+                    unlzma ./"$n"      ;;
+                application/x-rar)       
+                    unrar x -ad ./"$n" ;;
+                application/zip)       
+                    unzip ./"$n"       ;;
+                application/x-compress)         
+                    uncompress ./"$n"  ;;
+                application/x-7z-compressed|application/x-arj|application/vnd.ms-cab-compressed|application/vnd.debian.binary-package|application/x-iso9660-image|application/x-lzh-compressed|application/x-msi|application/x-rpm|application/x-xar)
+                    7za x ./"$n"        ;;
+                application/x-dosexec)       
+                    cabextract ./"$n"  ;;
+                application/x-cpio)      
+                    cpio -id < ./"$n"  ;;
+                application/octet-stream)
+                    # Fallback on file extension 
+                    name=$(basename -- "$n")
+                    ext=${FILENAME##*.}
+
+                    case "$ext" in
+                        chm|dmg|wim) 7za x ./"$n" ;;
+                        exe) cabextract ./"$n"   ;; # sometimes shows as octet-stream, otherwise is x-dosexec
+                    esac
+                    ;;
                 *)
                     echo "extract: '$n' - unknown archive method"
                     exit 1
